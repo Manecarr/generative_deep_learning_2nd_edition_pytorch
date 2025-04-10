8974810071ffc90bd4cda8f9c1a176bd909c5446
TTNecArr# <marco.arrigoni@outlook.de>
2025-04-10 10:51:43 +0200

    Update logging of metrics.

--- utils/metrics/metrics.py
+++ utils/metrics/metrics.py
@@ -28,3 +28,43 @@ def calculate_accuracy(logits: Tensor, labels: Tensor) -> float:
     correct_predictions = torch.sum(predictions == labels).item()
     accuracy: float = correct_predictions / labels.size(0)
     return accuracy
+
+
+def calculate_tp_fp_tn_fn(logits: Tensor, labels: Tensor) -> tuple[int, int, int, int, float, float, float, float]:
+    """Calculate the number of true positives, false positives, true negatives and false negatives.
+
+    Results will be reported both in absolute numbers and as a percentage of the total number of samples.
+
+    .. note::
+        If there are more than two classes, only TPs and FPs will be calculated.
+        The other metrics will be set to 0.
+
+    Args:
+        logits: logits tensor of shape (batch_size, num_classes).
+        labels: tensor of shape (batch_size, ) with the class ids.
+
+    Returns:
+        the metrics.
+    """
+    predictions = from_logits_to_class_ids(logits)
+    num_samples = labels.size(0)
+    if labels.unique().size(0) > 2:
+        tps = torch.sum(predictions == labels).item()
+        fps = torch.sum(predictions != labels).item()
+        tns = 0.0
+        fns = 0.0
+    else:
+        tps = torch.sum((predictions == 1) & (labels == 1)).item()
+        fps = torch.sum((predictions == 1) & (labels == 0)).item()
+        tns = torch.sum((predictions == 0) & (labels == 0)).item()
+        fns = torch.sum((predictions == 0) & (labels == 1)).item()
+    return (
+        int(tps),
+        int(fps),
+        int(tns),
+        int(fns),
+        tps / num_samples,
+        fps / num_samples,
+        tns / num_samples,
+        fns / num_samples,
+    )
