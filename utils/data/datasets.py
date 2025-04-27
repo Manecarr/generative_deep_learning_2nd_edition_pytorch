89519234ce4df5ed4628444096be27dcabebf4a7
TTNecArr# <marco.arrigoni@outlook.de>
2025-04-27 08:50:23 +0200

    Added tools for CelebA dataset.

--- utils/data/datasets.py
+++ utils/data/datasets.py
@@ -1,14 +1,22 @@
 """Utilities to deal with dataset."""
 
 from collections.abc import Callable
+import csv
 from functools import partial
+import logging
 from pathlib import Path
 from typing import Literal
+import zipfile
 
+from diskcache import Cache
+from PIL import Image
 import torch
 import torchvision
 
 DATA_CACHE_DIR = Path(__name__).parent.parent.joinpath(".cache_data").absolute()
+
+
+logger = logging.getLogger(__name__)
 
 
 def get_torchvision_dataset(
@@ -32,7 +40,7 @@ def get_torchvision_dataset(
         the dataset splits.
 
     Raises:
-        ValueError: if ``split`` is not a valid value.
+        ValueError: if ``split`` is not a valid value or if ``dataset_name`` is not supported.
     """
     obj_name: str
     match dataset_name:
@@ -84,3 +92,145 @@ get_fashion_mnist_dataset = partial(
     get_torchvision_dataset,
     dataset_name="fashion_mnist",
 )
+
+
+def get_kaggle_dataset(
+    dataset_name: str,
+    split: Literal["train", "val", "both"],
+    save_path: Path,
+    transform: Callable | None,
+) -> torch.utils.data.Dataset | tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
+    """Download a dataset from Kaggle.
+
+    Args:
+        dataset_name: the name of the dataset to load.
+        split: if 'train', returns the training set. If 'val', the validation set. If 'both', returns both.
+        save_path: where to save the dataset to disk, used only if ``download=True``.
+        transform: the transformation to apply to the dataset.
+
+    Returns:
+        the dataset splits.
+
+    Raises:
+        ValueError: if ``split`` is not a valid value or if ``dataset_name`` is not supported.
+    """
+    import kaggle as kg
+
+    def _download_and_unzip(kaggle_dataset_name: str, save_path: Path) -> dict[str, Path]:
+        """Download and unzip the Kaggle dataset. Returns the paths to the extracted files."""
+        kg.api.dataset_download_files(
+            dataset=kaggle_dataset_name, path=save_path, force=False, quiet=False, unzip=False
+        )
+        zip_file_paths = [x for x in save_path.glob("*.zip")]
+        assert len(zip_file_paths) == 1, f"Expected one zip file, found {len(zip_file_paths)}."
+        zip_file_path = zip_file_paths[0]
+        # Unzip the file
+        data_path = save_path.joinpath("data")
+        with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+            zip_ref.extractall(data_path)
+        extracted_files = [x for x in data_path.iterdir()]
+        return {x.name: x for x in extracted_files}
+
+    kg.api.authenticate()
+    match dataset_name:
+        case "celeba":
+            kaggle_dataset_name = "jessicali9530/celeba-dataset"
+        case _:
+            raise ValueError(f"Not supported dataset: {dataset_name}.")
+
+    logging.info(f"Attempting to download {kaggle_dataset_name} dataset from Kaggle.")
+    with Cache(save_path.joinpath("kaggle_cache")) as cache:
+        data_path = save_path.joinpath(kaggle_dataset_name)
+        if dataset_name in cache:
+            if data_path.exists():
+                logging.info(f"Dataset {kaggle_dataset_name} already downloaded, getting it from the cache.")
+                downloaded_files = cache[dataset_name]
+            else:
+                logging.info(
+                    f"Dataset {kaggle_dataset_name} should be already in the cache but was not found. "
+                    "It will be downlaoded again."
+                )
+                del cache[dataset_name]
+                cache[dataset_name] = downloaded_files = _download_and_unzip(kaggle_dataset_name, data_path)
+        else:
+            logging.info(
+                f"Downloading dataset {kaggle_dataset_name}. It will be added to the cache with key {dataset_name}."
+            )
+            cache[dataset_name] = downloaded_files = _download_and_unzip(kaggle_dataset_name, data_path)
+    match dataset_name:
+        case "celeba":
+            split_file_path = downloaded_files["list_eval_partition.csv"]
+            images_dir = downloaded_files["img_align_celeba"].joinpath("img_align_celeba")
+            match split:
+                case "train":
+                    return CelebADataset(split_file_path, images_dir, split, transform)
+                case "val":
+                    return CelebADataset(split_file_path, images_dir, split, transform)
+                case "both":
+                    return (
+                        CelebADataset(split_file_path, images_dir, "train", transform),
+                        CelebADataset(split_file_path, images_dir, "val", transform),
+                    )
+                case _:
+                    raise ValueError(f"Invalid split: {split}. Must be 'train', 'val' or 'both'.")
+        case _:
+            raise ValueError(f"Not supported dataset: {dataset_name}.")
+
+
+get_celeb_a_dataset = partial(get_kaggle_dataset, dataset_name="celeba")
+
+
+class CelebADataset(torch.utils.data.Dataset):
+    """CelebA dataset."""
+
+    def __init__(
+        self,
+        split_file_path: Path,
+        images_dir: Path,
+        split: Literal["train", "val", "test"],
+        transform: Callable | None = None,
+    ) -> None:
+        """Initialize the dataset."""
+        self.split_file_path = split_file_path
+        self.images_dir = images_dir
+        self.split = split
+        self.transform = transform
+
+        self.images_paths: list[Path] = self._get_images_paths()
+
+    def _get_images_paths(self) -> list[Path]:
+        """Get the paths to the images in the dataset for the desired split.
+
+        0: train
+        1: validation
+        2: test
+        """
+        match self.split:
+            case "train":
+                split_index = 0
+            case "val":
+                split_index = 1
+            case "test":
+                split_index = 2
+            case _:
+                raise ValueError(f"Invalid split: {self.split}. Must be 'train', 'validation' or 'test'.")
+        img_names: list[str] = []
+        with self.split_file_path.open("r", encoding="utf-8") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if int(row[1]) == split_index:
+                    img_names.append(row[0])
+        return [self.images_dir.joinpath(x) for x in img_names]
+
+    def __len__(self) -> int:
+        """Return the number of images in the split."""
+        return len(self.images_paths)
+
+    def __get_item__(self, index: int) -> torch.Tensor:
+        """Return a given image."""
+        image_path = self.images_paths[index]
+        image = Image.open(image_path)
+        image_tensor = torchvision.transforms.v2.ToImage()(image)
+        if self.transform:
+            image_tensor = self.transform(image_tensor)
+        return image_tensor
